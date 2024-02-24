212968ab7e333092fc58ca34d22d22029ac4c0aa
surajshivakumar <surajshivakumar53@gmail.com>
2024-02-24 15:38:13 -0500

    initialize again

---
+++ static/landing.js
@@ -0,0 +1,7 @@
+document.getElementById("learnButton").addEventListener("click", function () {
+  alert("Learn button clicked!");
+});
+
+document.getElementById("testButton").addEventListener("click", function () {
+  alert("Test button clicked!");
+});
